3ea137f84ce9c24a141f797a481c69892e4ab5fc
Marius <43277573+Marius@users.noreply.github.com>
2018-09-20 19:07:09 +0200

    Needs to be fixed

--- authen_login.php
+++ authen_login.php
@@ -1,15 +1,15 @@
-<?php  
+<?php
  require('db_connect.php');
 
 if (isset($_POST['username']) and isset($_POST['password'])){
-	
+
 // Assigning POST values to variables.
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 // CHECK FOR THE RECORD FROM TABLE
 $query = "SELECT * FROM `accounts` WHERE username='$username' and Password='$password'";
- 
+
 $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 $count = mysqli_num_rows($result);
 
@@ -17,6 +17,7 @@ if ($count == 1){
 
 //echo "Login Credentials verified";
 echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
+header("Location:login.php?p=1");
 
 }else{
 echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
